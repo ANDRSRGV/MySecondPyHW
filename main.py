a2f6becd72a8080ff44dfd3c6212d52e1eb82bb3
Andrey Sergeev <homemail45@gmail.com>
2022-07-13 16:54:05 +0300

    Task 3 done

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def rec_split(rec:str):
-    rec_list = rec.split('|')
-    rec_dict = {}
-    rec_dict['ingredient_name'] = rec_list[0].strip()
-    rec_dict['quantity'] = rec_list[1].strip()
-    rec_dict['measure'] = rec_list[2].strip()
-    return(rec_dict)
 
 with open('recipe.txt', 'r', encoding='utf-8') as data:
     cook_book = {}
@@ -19,16 +12,34 @@ with open('recipe.txt', 'r', encoding='utf-8') as data:
             if new_rec == True:
                 rec_name = line.strip()
                 cook_book.setdefault(rec_name)
-                rec_dishes_count = data.readline()
+                rec_ingr_count = data.readline()
                 new_rec = False
             else:
                 rec_list = []
                 rec_list = line.split('|')
                 rec_dict = {}
                 rec_dict['ingredient_name'] = rec_list[0].strip()
-                rec_dict['quantity'] = rec_list[1].strip()
+                rec_dict['quantity'] = int(rec_list[1].strip())
                 rec_dict['measure'] = rec_list[2].strip()
-                rec.append(rec_split(line))
+                rec.append(rec_dict)
     cook_book[rec_name] = rec
     print(cook_book)
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ing_dict = {}
+    for rec in dishes:
+        for ingr in cook_book[rec]:
+            ing_dict.setdefault(ingr['ingredient_name'], {'measure':ingr['measure'],'quantity':0})
+            ing_dict[ingr['ingredient_name']]['quantity'] += ingr['quantity'] * person_count
+
+    print(ing_dict)
+
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+
+
 
