183484567c431008815353d9e4d0cdb16dd48942
Andrey Sergeev <homemail45@gmail.com>
2022-07-14 10:39:49 +0300

    Task 3 - improved path make-up

--- task3.py
+++ task3.py
@@ -1,11 +1,11 @@
-
+import os
 
 file_set = ['1.txt', '2.txt', '3.txt']
-sub_dir='SortedFiles/'
+sub_dir='SortedFiles'
 file_len = []
 file_len_name = {}
 for file in file_set:
-    with open(sub_dir + file,'r', encoding='utf-8') as data:
+    with open(os.path.join(sub_dir, file),'r', encoding='utf-8') as data:
         text = data.read()
         lines = text.count('\n') + 1
         file_len.append(lines)
@@ -21,8 +21,7 @@ with open(sub_dir + 'sorted_files.txt','w', encoding='utf-8') as data:
     for file_pos in file_len:
         for file in file_len_name[file_pos]:
             output += f'{file}\n{file_pos}\n'
-            with open(sub_dir + file, 'r', encoding='utf-8') as cur_file:
+            with open(os.path.join(sub_dir, file), 'r', encoding='utf-8') as cur_file:
                 output += f'{cur_file.read()}\n\n'
     data.write(output)
-
 
